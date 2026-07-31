de67e0b2bf8ed42994b84d71624257c3b57b7ef4
Vansh745237 <vanshbathla125@gmail.com>
2026-07-31 14:22:15 +0530

    Initial commit

---
+++ react/vite project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Book from './component/Book'
+import Chemistry from './component/chemistry'
+import Mathematics from './component/mathematics'
+
+const App = () => {
+  return (
+    <div>
+      <Book />
+      <Chemistry/>
+      <Mathematics/>
+    </div>
+  )
+}
+
+export default App
